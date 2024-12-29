87ba981b9292fdfbf9e8ce4c1d198bf48a910592
Egor <osipov20020829@gmail.com>
2024-12-29 10:56:08 +0300

    srcond

--- precode.go
+++ precode.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 	"sync"
+	"sync/atomic"
+	"time"
 )
 
 // Generator генерирует последовательность чисел 1,2,3 и т.д. и
@@ -29,16 +31,25 @@ func Generator(ctx context.Context, ch chan<- int64, fn func(int64)) {
 // Worker читает число из канала in и пишет его в канал out.
 func Worker(in <-chan int64, out chan<- int64) {
 	// 2. Функция Worker
-	num := <-in
-	out <- num
-	close(out)
+	for {
+		val, ok := <-in
+
+		if !ok {
+			close(out)
+			return
+		}
+
+		out <- val
+
+		time.Sleep(time.Millisecond)
+	}
 }
 
 func main() {
 	chIn := make(chan int64)
 
 	// 3. Создание контекста
-	ctx, cancel := context.WithCancel(context.Background())
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
 
 	// для проверки будем считать количество и сумму отправленных чисел
@@ -47,8 +58,8 @@ func main() {
 
 	// генерируем числа, считая параллельно их количество и сумму
 	go Generator(ctx, chIn, func(i int64) {
-		inputSum += i
-		inputCount++
+		atomic.AddInt64(&inputSum, i)
+		atomic.AddInt64(&inputCount, 1)
 	})
 
 	const NumOut = 5 // количество обрабатывающих горутин и каналов
@@ -68,13 +79,15 @@ func main() {
 	var wg sync.WaitGroup
 
 	// 4. Собираем числа из каналов outs
-	for _, v := range outs {
-		var count int64 = 0
-		for ch := range v {
-			chOut <- ch
-			count ++
-		}
-		amounts = append(amounts, count)
+	for i, v := range outs {
+		wg.Add(1)
+		go func (in <-chan int64, i int64)  {
+			defer wg.Done()
+			for val := range in{		
+				chOut <- val
+				amounts[i]++
+			}
+		}(v, int64(i))
 	}
 
 	go func() {
